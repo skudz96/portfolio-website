e1da4c7a8235bd3aa8ad0169c777d0d16ba1df0b
Sergio <sergeh96@gmail.com>
2025-02-23 15:19:14 +0000

    fix: footer template changed

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <footer className="bg-highlight py-8">
       <div className="container mx-auto px-6 text-center">
-        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} Sergio Huleani. All rights reserved.
+        </p>
       </div>
     </footer>
   );
